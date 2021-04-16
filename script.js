2e43a0c2d94013ba9feaefbfaf1a3ff341b959cf
Ewan Green <ewan@EwanArch.localdomain>
2021-04-16 11:10:07 -0600

    click to select discord :)

--- script.js
+++ script.js
@@ -56,3 +56,21 @@ function includeHTML()
         }
     }
 };
+function selectElementText(el, win) {
+    win = win || window;
+    var doc = win.document, sel, range;
+    if (win.getSelection && doc.createRange) {
+        sel = win.getSelection();
+        range = doc.createRange();
+        range.selectNodeContents(el);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (doc.body.createTextRange) {
+        range = doc.body.createTextRange();
+        range.moveToElementText(el);
+        range.select();
+    }
+}
+
+selectElementText(document.getElementById("someElement"));
+selectElementText(elementInIframe, iframe.contentWindow);
