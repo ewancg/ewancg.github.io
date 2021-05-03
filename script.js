ee5493fa48841a478f3064041a065ab7ff257f8a
EwanGreen4 <ewangreen95@gmail.com>
2021-05-03 07:19:09 -0600

    make dem floating

--- script.js
+++ script.js
@@ -72,5 +72,46 @@ function selectElementText(el, win) {
     }
 }
 
+// Make the DIV element draggable:
+
+Array.from(document.getElementsByClassName("divPopup")).forEach(dragElement);
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elmnt.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
 selectElementText(document.getElementById("someElement"));
 selectElementText(elementInIframe, iframe.contentWindow);
+
+//shittons of this code was taken and changed from w3schools/stack overflow because i am a javascript noob and intend to keep it that way
