9bd0106932b96c13d6752eb8e9856aa3c44e9463
Ewan Green <ewangreen95@gmail.com>
2022-02-02 14:42:32 -0700

    part 2

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
-let width = screen.width;
+let width = window.innerWidth;
 let mobileTolerance = 680;
 
-function showPopup(id) 
-{
-	if(width > mobileTolerance) {
+function mobileStateChanged() {
+	if ((window.innerWidth > mobileTolerance && width <= mobileTolerance) || (window.innerWidth <= mobileTolerance && width > mobileTolerance))
+		return true;
+	else return false;
+}
+
+let resizeObserver = new ResizeObserver(() => {
+	if (mobileStateChanged()) {
+		hidePopup("all");
+		Array.from(document.getElementsByClassName("divPopup")).forEach(dragElement);
+	}
+	width = window.innerWidth;
+});
+
+function showPopup(id) {
+	if (width > mobileTolerance) {
 		document.getElementById(id).style.display = "block";
+		var w, h;
+		w = document.getElementById(id).offsetWidth.toString();
+		h = document.getElementById(id).offsetHeight.toString();
+		document.getElementById(id).style.minWidth = w.concat("px");
+		document.getElementById(id).style.maxWidth = w.concat("px");
+		document.getElementById(id).style.minHeight = h.concat("px");
+		document.getElementById(id).style.maxHeight = h.concat("px");
 	} else {
 		document.getElementById(id).style.display = "flex";
 	}
 	document.getElementById("divPopupGray").style.display = "block";
 	
-	var w, h;
-	w = document.getElementById(id).offsetWidth.toString();
-	h = document.getElementById(id).offsetHeight.toString();
-	document.getElementById(id).style.minWidth = w.concat("px");
-	document.getElementById(id).style.maxWidth = w.concat("px");
-	document.getElementById(id).style.minHeight = h.concat("px");
-	document.getElementById(id).style.maxHeight = h.concat("px");
 };
 
-function hidePopup(id) 
-{
-	if(id == "all")
-	{
+function hidePopup(id) {
+	if (id == "all") {
 		var els = document.querySelectorAll('.divPopup');
-		for (var i=0; i < els.length; i++) {
+		for (var i = 0; i < els.length; i++) {
 			els[i].setAttribute("style", "display", "none");
 			els[i].setAttribute("style", "left", "50%");
 			els[i].setAttribute("style", "top", "50%");
 		}
-	}
-	else
-	{
+	} else {
 		document.getElementById(id).style.display = "none";
 		document.getElementById(id).style.left = "50%";
 		document.getElementById(id).style.top = "50%";
@@ -39,48 +48,10 @@ function hidePopup(id)
 	document.getElementById("divPopupGray").style.display = "none";
 };
 
-function includeHTML()
-{
-	var z, i, elmnt, file, xhttp;
-	/*loop through a collection of all HTML elements:*/
-	z = document.getElementsByTagName("*");
-	for (i = 0; i < z.length; i++)
-	{
-		elmnt = z[i];
-		/*search for elements with a certain atrribute:*/
-		file = elmnt.getAttribute("include-html");
-		if (file)
-		{
-			/*make an HTTP request using the attribute value as the file name:*/
-			xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function()
-			{
-				if (this.readyState == 4)
-				{
-					if (this.status == 200)
-					{
-						elmnt.innerHTML = this.responseText;
-					}
-					if (this.status == 404)
-					{
-						elmnt.innerHTML = "Page not found.";
-					}
-					/*remove the attribute, and call this function once more:*/
-					elmnt.removeAttribute("include-html");
-					includeHTML();
-				}
-			}      
-			xhttp.open("GET", file, true);
-			xhttp.send();
-			/*exit the function:*/
-			return;
-		}
-	}
-};
-
 function selectElementText(el, win) {
 	win = win || window;
-	var doc = win.document, sel, range;
+	var doc = win.document,
+	sel, range;
 	if (win.getSelection && doc.createRange) {
 		sel = win.getSelection();
 		range = doc.createRange();
@@ -96,11 +67,17 @@ function selectElementText(el, win) {
 
 // Make the DIV element draggable:
 
-Array.from(document.getElementsByClassName("divPopup")).forEach(dragElement);
-
 function dragElement(elmnt) {
-	if(width > mobileTolerance) {
-		var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	if (width < mobileTolerance) {
+		elmnt.onmousedown = null;
+		document.onmouseup = null;
+		document.onmousemove = null;
+	} else {
+		
+		var pos1 = 0,
+		pos2 = 0,
+		pos3 = 0,
+		pos4 = 0;
 		elmnt.onmousedown = dragMouseDown;
 		
 		function dragMouseDown(e) {
@@ -134,5 +111,8 @@ function dragElement(elmnt) {
 		}
 	}
 };
+
+resizeObserver.observe(document.body);
+Array.from(document.getElementsByClassName("divPopup")).forEach(dragElement);
 
 //shittons of this code was taken and changed from w3schools/stack overflow because i am a javascript noob and intend to keep it that way
