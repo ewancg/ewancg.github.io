a8b71d1ba9c7b7f746d8755bc9e10b9031fe00d5
Ewan Green <ewangreen95@gmail.com>
2022-06-03 21:42:55 -0600

    only drag windows if it's the left button pressed

--- script.js
+++ script.js
@@ -64,8 +64,6 @@ function selectElementText(el, win) {
   }
 };
 
-// Make the DIV element draggable:
-
 function dragElement(elmnt) {
     var pos1 = 0,
       pos2 = 0,
@@ -77,30 +75,30 @@ function dragElement(elmnt) {
 	if (width <= mobileTolerance) return;
       e = e || window.event;
       e.preventDefault();
-      // get the mouse cursor position at startup:
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      document.onmouseup = closeDragElement;
-      // call a function whenever the cursor moves:
-      document.onmousemove = elementDrag;
+
+      if(e.button == 0) {
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+      }
     }
 
     function elementDrag(e) {
 	if (width <= mobileTolerance) return;
       e = e || window.event;
       e.preventDefault();
-      // calculate the new cursor position:
+
       pos1 = pos3 - e.clientX;
       pos2 = pos4 - e.clientY;
       pos3 = e.clientX;
       pos4 = e.clientY;
-      // set the element's new position:
+      
       elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
       elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
     }
 
     function closeDragElement() {
-      // stop moving when mouse button is released:
       document.onmouseup = null;
       document.onmousemove = null;
     }
@@ -108,5 +106,3 @@ function dragElement(elmnt) {
 
 resizeObserver.observe(document.body);
 Array.from(document.getElementsByClassName("divPopup")).forEach(dragElement);
-
-//shittons of this code was taken and changed from w3schools/stack overflow because i am a javascript noob and intend to keep it that way
