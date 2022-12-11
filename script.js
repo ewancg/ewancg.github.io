ccfc68c9fd42e24579623080b3ee49a29dd41c62
Ewan Green <ewangreen95@gmail.com>
2022-12-10 19:15:58 -0700

    redesign

--- script.js
+++ script.js
@@ -1,12 +1,85 @@
 let width = window.innerWidth;
 let mobileTolerance = 680;
 
+function dockNav(docked) {
+  let content = document.getElementsByClassName("divContent")[0];
+/*   let fn = docked ? content.setAttribute : content.removeAttribute;
+  fn.call(content, "docked", "true") */
+
+  content.setAttribute("docked", docked);
+}
+
+function showContact() {
+  dockNav(true);
+}
+
+function showPortfolio() {
+  dockNav(true);
+}
+
+function showSummary() {
+  dockNav(false);
+}
+
+function animateNavButton(parent, object, index, count) {
+  let step = 100 / count;
+  const style = document.createElement('style');
+  style.textContent = `
+  #${parent.id} {
+      background-size: ${(step)}% 100%;
+      background-position-x: ${(50 * index)}%;
+    }
+    `;
+    document.head.appendChild(style);
+    Array.from(parent.children).forEach(function (child) {
+      child.setAttribute("selected", false);
+    })
+    object.setAttribute("selected", true);
+}
+
+function createNavButton(obj) {
+  let children = obj.children;
+  for (let i = 0; i < children.length; i++) {
+    let z = i;
+    children[i].addEventListener("mousedown", function () { animateNavButton(obj, this, z, children.length) })
+  }
+
+  animateNavButton(obj, children[0], 0, children.length);
+}
+
+let navButtonContainers = document.getElementsByClassName("navButtonContainer");
+for (let i = 0; i < navButtonContainers.length; i++) {
+  createNavButton(navButtonContainers[i]);
+}
+
+document.onload = () => {
+
+  Array.from(document.getElementsByClassName("autoAnimate")).forEach((item) => {
+    let x = item.getAnimations()
+    
+    x.forEach((animation) => {
+      animation.onfinish = ()=> {
+        console.log("e");
+      }
+    }) 
+  })
+  
+}
+
+
+
+
+
+
+
+
+
 function mobileStateChanged() {
   return ((window.innerWidth > mobileTolerance && width <= mobileTolerance) || (window.innerWidth <= mobileTolerance && width > mobileTolerance));
 }
 
 let resizeObserver = new ResizeObserver(() => {
-	if(width == window.innerWidth) return;
+  if (width == window.innerWidth) return;
   if (mobileStateChanged()) {
     hidePopup("all");
     Array.from(document.getElementsByClassName("divPopup")).forEach(dragElement);
@@ -65,43 +138,43 @@ function selectElementText(el, win) {
 };
 
 function dragElement(elmnt) {
-    var pos1 = 0,
-      pos2 = 0,
-      pos3 = 0,
-      pos4 = 0;
-    elmnt.onmousedown = dragMouseDown;
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  elmnt.onmousedown = dragMouseDown;
 
-    function dragMouseDown(e) {
-	if (width <= mobileTolerance) return;
-      e = e || window.event;
-      e.preventDefault();
+  function dragMouseDown(e) {
+    if (width <= mobileTolerance) return;
+    e = e || window.event;
+    e.preventDefault();
 
-      if(e.button == 0) {
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-      }
-    }
-
-    function elementDrag(e) {
-	if (width <= mobileTolerance) return;
-      e = e || window.event;
-      e.preventDefault();
-
-      pos1 = pos3 - e.clientX;
-      pos2 = pos4 - e.clientY;
+    if (e.button == 0) {
       pos3 = e.clientX;
       pos4 = e.clientY;
-      
-      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
     }
+  }
 
-    function closeDragElement() {
-      document.onmouseup = null;
-      document.onmousemove = null;
-    }
+  function elementDrag(e) {
+    if (width <= mobileTolerance) return;
+    e = e || window.event;
+    e.preventDefault();
+
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 };
 
 resizeObserver.observe(document.body);
