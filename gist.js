3698db5d3024daf3a7fc455a9426bad50029ed55
Ewan Green <ewangreen95@gmail.com>
2022-12-18 10:53:02 -0700

    crude dropdown

--- gist.js
+++ gist.js
@@ -23,45 +23,102 @@ document.addEventListener('DOMContentLoaded', async function () {
     if (metaXhr.status == 200) {
       let response = JSON.parse(metaXhr.responseText);
       response.forEach((gist) => {
-        for (let key in gist.files) {
-          const dataXhr = new XMLHttpRequest();
-          dataXhr.open("GET", gist.files[key].raw_url);
-          dataXhr.send();
-          dataXhr.onload = () => {
-            const container = document.getElementById("portfolioPage");
 
+        let dropdown = document.createElement("div");
+        dropdown.classList.toggle("dropdown", true);
+        
+        let header = document.createElement("div");
+        header.classList.toggle("dropdownHeader", true);
 
-            // gist dropdown, somewhere
-            let code = document.createElement("pre");
-            code.classList.toggle("codeContainer", 1);
+        
+        let dropdownTitle = document.createElement("p");
+        dropdownTitle.classList.toggle("uiText", true);
+        dropdownTitle.classList.toggle("dropdownTitle", true);
+        dropdownTitle.innerText = gist.description;
+        header.appendChild(dropdownTitle);
 
-            let cleanHTML = html => { return html.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;'); }
-            let text = cleanHTML(dataXhr.responseText);
+        let dropdownIcon = document.createElement("img");
+        dropdownIcon.classList.toggle("dropdownIcon", true);
+        dropdownIcon.src = "./resource/favicon.png"
+        header.appendChild(dropdownIcon);
+        dropdown.appendChild(header);
 
-            code.innerHTML = text;
-            code.setAttribute("data-lang", languageAliases.get(gist.files[key].language));
-            monaco.editor.colorizeElement(code, theme).then(html => {
-              container.appendChild(code);
-            });
+        const page = document.getElementById("portfolioPage");
+        page.appendChild(dropdown);
 
-            /*
-            dest.getElementsByClassName("locCounter")[0].innerText = dataXhr.responseText.split(/\r\n|\r|\n/).length - 1;
-            dest.getElementsByClassName("sizeCounter")[0].innerText = `${gist.files[key].size} bytes`;
-            dest.getElementsByClassName("filename")[0].innerText = gist.files[key].filename;
-            dest.getElementsByClassName("description")[0].innerText = gist.description;
-            */
-
-            console.log(
-              `
-                        ${gist.description.trim()};
-                        ${gist.files[key].filename}; ${gist.files[key].language} (${languageAliases.get(gist.files[key].language)});
-                        ${dataXhr.responseText.split(/\r\n|\r|\n/).length - 1} lines, ${gist.files[key].size} bytes
-                        `)
-
-            // placeholder
-            // also set document description, filename, filesize, and language, set up prismjs
+        dropdown.addEventListener("click", () => {
+          for (let key in gist.files) {
+            const dataXhr = new XMLHttpRequest();
+            dataXhr.open("GET", gist.files[key].raw_url);
+            dataXhr.send();
+            dataXhr.onload = () => {
+              const destination = document.createElement("div");
+              destination.classList.toggle("dropdownContents", true)
+              dropdown.appendChild(destination);
+              dropdown.setAttribute("expanded" , true);
+  
+              // gist dropdown, somewhere
+              let container = document.createElement("div");
+              container.classList.toggle("codeContainer", true);
+              let code = document.createElement("pre");
+              code.classList.toggle("gistCode", true);
+              
+              let cleanHTML = html => { return html.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;'); }
+              let text = cleanHTML(dataXhr.responseText);
+              
+              let footer = document.createElement("div");
+              footer.classList.toggle("codeFooter", true);
+              
+              code.innerHTML = text;
+              code.setAttribute("data-lang", languageAliases.get(gist.files[key].language));
+              monaco.editor.colorizeElement(code, theme).then(html => {
+                container.appendChild(code);
+                destination.appendChild(container);
+                container.appendChild(footer);
+              });
+  
+              // Footer
+              let filename = document.createElement("p");
+              filename.classList.toggle("footerText", true);
+              filename.innerText = gist.files[key].filename.trim();
+  
+              let lang = document.createElement("p");
+              lang.classList.toggle("footerText", true);
+              lang.innerText = gist.files[key].language;
+              lang.style = "flex: 1 0 0;"
+  
+              let lines = document.createElement("p");
+              lines.classList.toggle("footerText", true);
+              lines.innerText = `${dataXhr.responseText.split(/\r\n|\r|\n/).length - 1} lines`;
+              
+              let size = document.createElement("p");
+              size.classList.toggle("footerText", true);
+              size.innerText = `${gist.files[key].size} bytes`;
+                          
+              footer.appendChild(filename);
+              footer.appendChild(lang);
+              footer.appendChild(lines);
+              footer.appendChild(size);
+              /*
+              dest.getElementsByClassName("locCounter")[0].innerText = dataXhr.responseText.split(/\r\n|\r|\n/).length - 1;
+              dest.getElementsByClassName("sizeCounter")[0].innerText = `${gist.files[key].size} bytes`;
+              dest.getElementsByClassName("filename")[0].innerText = gist.files[key].filename;
+              dest.getElementsByClassName("description")[0].innerText = gist.description;
+              */
+  
+              console.log(
+                `
+                          ${gist.description.trim()};
+                          ${gist.files[key].filename}; ${gist.files[key].language} (${languageAliases.get(gist.files[key].language)});
+                          ${dataXhr.responseText.split(/\r\n|\r|\n/).length - 1} lines, ${gist.files[key].size} bytes
+                          `)
+  
+              // placeholder
+              // also set document description, filename, filesize, and language, set up prismjs
+            }
           }
-        }
+        })
+        
       });
     } else {
       console.log("Unable to access the GitHub API for Gists.");
